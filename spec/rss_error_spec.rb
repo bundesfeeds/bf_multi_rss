f3cbf1a26bdbe52df1665c4715d398c602dc6ddb
Sebastian Schürmann <sebs@2xs.org>
2017-03-23 09:25:10 +0100

    Error handler

--- spec/rss_error_spec.rb
+++ spec/rss_error_spec.rb
@@ -1,3 +1,16 @@
 require 'spec_helper'
 RSpec.describe BfMultiRss::RssError do
+
+  describe 'properties' do
+    let(:err) do
+      BfMultiRss::RssError.new('http://foo', Object)
+    end
+    it 'uri' do
+      expect(err.uri).to eq('http://foo')
+    end
+    it 'e' do
+      expect(err.e).to eq(Object)
+    end
+
+  end
 end
